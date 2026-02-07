d3e152bf519944b6b7a176ba9fc3262f90b38da9
Matheus Bueno <matheussbueno10@hotmail.com>
2026-02-07 01:42:59 -0300

    Create Livro.java

---
+++ modulo2/Livro.java
@@ -0,0 +1,30 @@
+public class Livro {
+    private String titulo;
+    private Autor autor;
+    private String genero;
+    private boolean disponivel = true;
+
+    public Livro(String titulo, Autor autor, String genero) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+    }
+
+    public String getTitulo() { return titulo; }
+    public Autor getAutor() { return autor; }
+    public String getGenero() { return genero; }
+    
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void validarDisponibilidade() {
+        if (!disponivel) {
+            System.out.println("O livro não está disponível");
+        }
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+}
